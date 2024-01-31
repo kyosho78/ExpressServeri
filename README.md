de9be409cfd590b0214a191b437391b3607ebab4
Valter Backström <dylandante@gmail.com>
2024-01-31 18:27:39 +0100

    Simon koodit

---
+++ README.md
@@ -0,0 +1,9 @@
+## Annettavat komennot projektin juurikansiossa:
+
+### npm init ja enterillä kuittaus kysymyksiin tai voi kirjoittaa jotain jos haluaa.
+
+### npm i express (sama kuin jos antaisi komennon npm install express)
+
+### Sovellus ajetaan: node server.js
+
+### Jos lataat projektin githubista, anna komento juurikansiossa: npm install, jolloin sopivat node moduulit asennetaan package.json tiedoston perusteella
--- server.js
+++ server.js
@@ -1,23 +1,39 @@
-//Express.js palvelin//
+// Express.js palvelin //
 const path = require('path')
 const express = require('express')
+const fs = require('fs').promises
 
 const app = express()
 
-//JSON Tiedoston importtaus
 const herkut = require('./herkut.json')
 
-//GET ALL etsitään kaikki namit jsonisya, controlleri metodi
+//GET ALL etsitään kaikki namit jsonista
 app.get('/api/herkut', (req, res) => {
     res.json(herkut)
     })
 
-//Tehdään polkumääritys public kansioon 
+
+ // Pinkoodin lukeminen txt tiedostosta pelvelimelta ja lähettäminen selaimelle   
+ app.get('/api/getpin', async (req, res) => {
+    try {
+        // Read the content of the text file
+        const savedPin = await fs.readFile('./pin.txt', 'utf-8')
+        
+        // Send the file content as the response
+        res.send(savedPin)
+    } catch (error) {
+        console.error('Error reading file:', error)
+        res.status(500).send('Internal Server Error')
+    }
+    })
+
+//-------------------------------------------------------
+// Tehdään polkumääritys public kansioon 
 const polku = path.join(__dirname, './public')
 
-//Sanotaan että em. polussa on tiedostonsisältöä jota palvelin käyttää kun se saa http request
+// Sanotaan että em. polussa on tiedostosisältö jota palvelin käyttää kun se saa http requestin
 app.use(express.static(polku))
 
-app.listen(3001, () => {
-    console.log('Server is up on port 3001.')
-}) 
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
+})
