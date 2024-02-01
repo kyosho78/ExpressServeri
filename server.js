1a6007a4075cd6a9ef0c34d4aeea8afc21f562a0
Valter Backström <dylandante@gmail.com>
2024-02-01 08:49:52 +0100

    muutoksia

--- server.js
+++ server.js
@@ -17,10 +17,12 @@ app.get('/api/herkut', (req, res) => {
  app.get('/api/getpin', async (req, res) => {
     try {
         // Read the content of the text file
-        const savedPin = await fs.readFile('./pin.txt', 'utf-8')
+        const savedPin = await fs.readFile('./pin.txt')
         
         // Send the file content as the response
-        res.send(savedPin)
+        // res.send(savedPin)
+        res.json({ pinkoodi: savedPin });
+
     } catch (error) {
         console.error('Error reading file:', error)
         res.status(500).send('Internal Server Error')
@@ -34,6 +36,6 @@ const polku = path.join(__dirname, './public')
 // Sanotaan että em. polussa on tiedostosisältö jota palvelin käyttää kun se saa http requestin
 app.use(express.static(polku))
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(4000, () => {
+    console.log('Server is up on port 4000.')
 })
